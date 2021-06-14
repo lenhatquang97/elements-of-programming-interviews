b833bbeb1c8387ebc551649ced6be09f862209dc
lenhatquang97 <lenhatquang97@gmail.com>
2021-06-14 09:38:57 +0700

    2 exercises today :))

--- arrays/src/main/java/AdvanceThroughArray.java
+++ arrays/src/main/java/AdvanceThroughArray.java
@@ -8,6 +8,8 @@ public class AdvanceThroughArray {
     public static boolean arrayAdvanceFoo(List<Integer> A,int start){
         if(start==A.size()-1)
             return true;
+        else if(start>=A.size())
+            return false;
         for(int i=A.get(start);i>=1;i--){
             return arrayAdvanceFoo(A,start+i);
         }
