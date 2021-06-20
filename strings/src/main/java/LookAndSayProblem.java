de713d89f3b35aed79ca47841b831675ab3847ca
lenhatquang97 <lenhatquang97@gmail.com>
2021-06-20 10:34:08 +0700

    Another three exercise

--- strings/src/main/java/LookAndSayProblem.java
+++ strings/src/main/java/LookAndSayProblem.java
@@ -3,10 +3,29 @@ public class LookAndSayProblem {
     /*
     7.8
     */
-
     public static String lookAndSay(int n) {
+        if(n==1)
+            return "1";
 
-        return "";
+
+        int start=0;
+        int end=0;
+        StringBuilder res=new StringBuilder("1");
+        for(int step=2;step<=n;step++){
+            StringBuilder first=new StringBuilder(res.toString());
+            res.delete(0,res.length()-1);
+            for(int i=1;i<first.length();i++){
+                if(first.charAt(i)!=first.charAt(start)){
+                    end=i-1;
+                    int tempCount=end-start+1;
+                    res.append(String.valueOf(tempCount)+first.charAt(i));
+                    start=i;
+                }
+            }
+            int anotherTemp=first.length()-start;
+            res.append(String.valueOf(anotherTemp)+first.charAt(first.length()-1));
+        }
+        return res.toString();
     }
 
 }
--- strings/src/main/java/ReplaceAndRemove.java
+++ strings/src/main/java/ReplaceAndRemove.java
@@ -5,8 +5,13 @@ public class ReplaceAndRemove {
     */
 
     public static String replaceAndRemove(char[] s, int k) {
-
+        int countA=0;
+        for(int i=0;i<k;i++){
+            s[k]=(s[k]=='b')?' ':s[k];
+            countA+=(s[k]=='a')?1:0;
+        }
         return s.toString();
     }
+
 
 }
