8bc15b2f0653d4fdbf4cd190655d72b4647dbc33
lenhatquang97 <lenhatquang97@gmail.com>
2021-06-21 09:44:15 +0700

    Done

--- strings/README.md
+++ strings/README.md
@@ -3,13 +3,13 @@
 - [x] 7.1 InterconvertStringAndInteger
 - [x] 7.2 BaseConversion
 - [x] 7.3 SpreadsheetColumnEncoding
-- [ ] 7.4 ReplaceAndRemove
 - [x] 7.5 PalindromeAlphanumeric
 - [x] 7.6 ReverseWordsInASentence
-- [ ] 7.7 ComputeMnemonicsPhoneNumber
 - [x] 7.8 LookAndSayProblem
 - [x] 7.9 RomanToDecimal
-- [ ] 7.10 ComputeValidIPAddresses
 - [x] 7.11 StringSinusoidal
-- [ ] 7.12 RunLengthEncoding
+- [x] 7.12 RunLengthEncoding
+- [ ] 7.4 ReplaceAndRemove
 - [ ] 7.13 FindFirstOccurrenceOfSubstring
+- [ ] 7.7 ComputeMnemonicsPhoneNumber: Backtracking
+- [ ] 7.10 ComputeValidIPAddresses: Backtracking
--- strings/src/main/java/RunLengthEncoding.java
+++ strings/src/main/java/RunLengthEncoding.java
@@ -1,3 +1,5 @@
+import java.util.Collections;
+
 public class RunLengthEncoding {
 
     /*
@@ -5,13 +7,31 @@ public class RunLengthEncoding {
     */
 
     public static String encode(String s) {
-
-        return "";
+        StringBuilder res=new StringBuilder();
+        if(s.length()==1){
+            return '1'+s;
+        }
+        int count=1;
+        for(int i=1;i<s.length();i++){
+            if(s.charAt(i)==s.charAt(i-1))
+                count+=1;
+            else{
+                res.append(String.valueOf(count)+s.charAt(i-1));
+                count=1;
+            }
+        }
+        res.append(String.valueOf(count)+s.charAt(s.length()-1));
+        return res.toString();
     }
 
     public static String decode(String s) {
-
-        return "";
+        StringBuilder res=new StringBuilder();
+        for(int i=0;i<s.length()/2;i++){
+            int val=s.charAt(2*i)-'0';
+            for(int k=1;k<=val;k++){
+                res.append(s.charAt(2*i+1));
+            }}
+        return res.toString();
     }
 
 }
