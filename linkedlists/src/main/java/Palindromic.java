1fbc28a6d18c01da425c7738bacfb9961ae5f506
lenhatquang97 <lenhatquang97@gmail.com>
2021-06-26 14:44:03 +0700

    Done palindrome :))

--- linkedlists/src/main/java/Palindromic.java
+++ linkedlists/src/main/java/Palindromic.java
@@ -5,8 +5,32 @@ public class Palindromic {
     */
 
     public static boolean isPalindromic(ListNode<Integer> list) {
+        ListNode<Integer>head=new ListNode<>(null);
+        ListNode<Integer>node=list;
+        if(node==null||node.next==null){
+            return true;
+        }
+        while(node!=null){
+            if(head==null){
+                head.data=node.data;
+                head.next=null;
+            }
+            else{
+                ListNode<Integer>temp=new ListNode<>(node.data);
+                temp.next=head;
+                head=temp;
+            }
+            node=node.next;
+        }
+        ListNode<Integer>first=list,second=head;
+        while(first!=null){
+            if(first.data!=second.data)
+                return false;
+            first=first.next;
+            second=second.next;
+        }
 
-        return false;
+        return true;
     }
 
 }
