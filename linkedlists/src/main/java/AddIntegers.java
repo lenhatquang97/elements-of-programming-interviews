3e80591d5ffc6f6f8b876c5ee3eee2c61e3e522e
lenhatquang97 <lenhatquang97@gmail.com>
2021-06-27 08:36:28 +0700

    Done :))

--- linkedlists/src/main/java/AddIntegers.java
+++ linkedlists/src/main/java/AddIntegers.java
@@ -5,7 +5,27 @@ public class AddIntegers {
     */
     
     public static ListNode<Integer> addIntegers(ListNode<Integer> a, ListNode<Integer> b) {
-
-        return new ListNode<>(null);
+        ListNode<Integer> res = null;
+        ListNode<Integer> pointer_res=res;
+        int tmpRem=0;
+        while(a!=null&&b!=null){
+            int tmpVal=(a.data+b.data)%10;
+            if(res==null){
+                res=new ListNode<>(tmpVal%10+tmpRem,null);
+                pointer_res=res;
+            }
+            else{
+                pointer_res.next=new ListNode<>(tmpVal%10+tmpRem,null);
+                pointer_res=pointer_res.next;
+            }
+            tmpRem=(a.data+b.data)/10;
+            a=a.next;
+            b=b.next;
+        }
+        if(tmpRem>=1){
+            pointer_res.next=new ListNode<>(tmpRem,null);
+            pointer_res=pointer_res.next;
+        }
+        return res;
     }
 }
--- linkedlists/src/main/java/TestForOverlappingLists.java
+++ linkedlists/src/main/java/TestForOverlappingLists.java
@@ -3,9 +3,30 @@ public class TestForOverlappingLists {
     /*
     8.4
     */
-
+    public static int length(ListNode<Integer>list){
+        int count=0;
+        while (list!=null){
+            count+=1;
+            list=list.next;
+        }
+        return count;
+    }
     public static ListNode<Integer> doListsOverlap(ListNode<Integer> list1, ListNode<Integer> list2) {
-
-        return new ListNode<>(null);
+        int a1=length(list1);
+        int a2=length(list2);
+        ListNode<Integer> p1=list1,p2=list2;
+        while(a1>a2){
+            p1=p1.next;
+            a1-=1;
+        }
+        while(a1<a2){
+            p2=p2.next;
+            a2-=1;
+        }
+        while (p1!=null&&p2!=null && p1!=p2){
+            p1=p1.next;
+            p2=p2.next;
+        }
+        return p1;
     }
 }
