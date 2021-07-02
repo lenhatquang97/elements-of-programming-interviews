ec8b3c73fe4b27b60405b939f80c21fb543d167c
lenhatquang97 <lenhatquang97@gmail.com>
2021-07-02 14:08:20 +0700

    Done :))

---
+++ stacksandqueues/src/main/java/Pairer.java
@@ -0,0 +1,8 @@
+public class Pairer {
+    public int value;
+    public int times;
+    public Pairer(int a,int b){
+        this.value=a;
+        this.times=b;
+    }
+}
--- stacksandqueues/src/main/java/StackWithMax.java
+++ stacksandqueues/src/main/java/StackWithMax.java
@@ -1,22 +1,48 @@
+import java.util.Stack;
+//Stack with Max xet nhung gia tri lon nhat, lon thu hai,... tro lai, so lan thay doi max chinh la so stack
 public class StackWithMax {
-
-    /*
-    9.1
-    */
-
-    public StackWithMax() {
-
-    }
+    private Stack<Integer> normalStack = new Stack<>();
+    private Stack<Pairer> stackedWithMax=new Stack<>();
+    public StackWithMax() {}
 
     public Integer max() {
-        return 0;
+        if(normalStack.isEmpty()) {
+            throw new IllegalStateException("max(): empty stack");
+        }
+        return stackedWithMax.peek().value;
     }
 
     public Integer pop() {
-        return 0;
+        if(normalStack.isEmpty()) {
+            throw new IllegalStateException("max(): empty stack");
+        }
+        Integer val=normalStack.pop();
+        if(val==stackedWithMax.peek().value){
+            Pairer newPair=stackedWithMax.pop();
+            if(newPair.times>0){
+                newPair.times--;
+                stackedWithMax.push(newPair);
+            }
+        }
+        return val;
     }
 
     public void push(Integer x) {
-
+        if(!normalStack.isEmpty()){
+            if(x==stackedWithMax.peek().value){
+                Pairer newPair = new Pairer(x,stackedWithMax.peek().times+1);
+                stackedWithMax.pop();
+                stackedWithMax.push(newPair);
+            }
+            else if(x>stackedWithMax.peek().value){
+                Pairer newPair = new Pairer(x,1);
+                stackedWithMax.push(newPair);
+            }
+        }
+        else{
+            Pairer newPair = new Pairer(x,1);
+            stackedWithMax.push(newPair);
+        }
+        normalStack.push(x);
     }
 }
