24575faa62701d9cc8391f627805b3eb738cdaaf
lenhatquang97 <lenhatquang97@gmail.com>
2021-07-02 14:49:54 +0700

    Done :))

--- stacksandqueues/README.md
+++ stacksandqueues/README.md
@@ -3,10 +3,10 @@
 - [x] 9.1 StackWithMax
 - [x] 9.2 EvaluateRPNExpressions
 - [x] 9.3 IsStringWellFormed
+- [x] 9.7 ComputeBinaryTreeNodes
 - [ ] 9.4 NormalizedPathnames
 - [ ] 9.5 SearchPostingsList
 - [ ] 9.6 ComputeBuildingsWithView
-- [ ] 9.7 ComputeBinaryTreeNodes
 - [ ] 9.8 CircularQueue
 - [ ] 9.9 QueueWithStacks
 - [ ] 9.10 QueueWithMax
--- stacksandqueues/src/main/java/ComputeBinaryTreeNodes.java
+++ stacksandqueues/src/main/java/ComputeBinaryTreeNodes.java
@@ -1,6 +1,12 @@
-import java.util.Collections;
-import java.util.List;
-
+import java.util.*;
+class Pair<T,V>{
+    public T a;
+    public V b;
+    public Pair(T first, V second){
+        this.a=first;
+        this.b=second;
+    }
+}
 public class ComputeBinaryTreeNodes {
 
     /*
@@ -8,7 +14,29 @@ public class ComputeBinaryTreeNodes {
     */
 
     public static List<List<Integer>> binaryTreeDepthOrder(BinaryTree<Integer> tree) {
-
-        return Collections.emptyList();
+        if(tree==null) return Collections.emptyList();
+        List<Integer>firstArr=new ArrayList<>();
+        List<List<Integer>> result = new ArrayList<>();
+        Queue<Pair<BinaryTree,Integer>> queue=new LinkedList<>();
+        BinaryTree<Integer>node= tree;
+        queue.add(new Pair(node,1));
+        int layer=1;
+        while(queue.peek()!=null){
+            if(queue.peek().b!=layer){
+                result.add(firstArr);
+                firstArr=new ArrayList<>();
+                layer=queue.peek().b;
+            }
+            if(queue.peek().a.left!=null){
+                queue.add(new Pair(queue.peek().a.left,queue.peek().b+1));
+            }
+            if(queue.peek().a.right!=null){
+                queue.add(new Pair(queue.peek().a.right,queue.peek().b+1));
+            }
+            firstArr.add((Integer)queue.peek().a.data);
+            queue.remove();
+        }
+        result.add(firstArr);
+        return result;
     }
 }
