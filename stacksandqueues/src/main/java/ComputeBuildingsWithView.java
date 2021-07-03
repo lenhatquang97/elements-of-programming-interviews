6c04e5a0d65f9adbe55538b517c902c39599f6a1
lenhatquang97 <lenhatquang97@gmail.com>
2021-07-03 15:51:01 +0700

    Done :))

--- stacksandqueues/src/main/java/ComputeBuildingsWithView.java
+++ stacksandqueues/src/main/java/ComputeBuildingsWithView.java
@@ -1,6 +1,7 @@
 import java.util.Deque;
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.Stack;
 
 public class ComputeBuildingsWithView {
 
@@ -9,7 +10,26 @@ public class ComputeBuildingsWithView {
     */
 
     public static Deque<BuildingWithHeight> examineBuildingsWithSunset(Iterator<Integer> sequence) {
+        Stack<BuildingWithHeight> stack=new Stack<>();
+        Deque<BuildingWithHeight> res=new LinkedList<>();
+        int id=0;
+        while(sequence.hasNext()){
+            BuildingWithHeight obj=new BuildingWithHeight(id,sequence.next());
+            stack.add(obj);
+            id++;
+        }
+        BuildingWithHeight tmp=stack.peek();
+        res.add(tmp);
+        int maxValue=tmp.height;
+        stack.pop();
+        while(!stack.isEmpty()){
+            if(stack.peek().height>maxValue){
+                maxValue=stack.peek().height;
+                res.add(stack.peek());
+            }
+            stack.pop();
+        }
 
-        return new LinkedList<>();
+        return res;
     }
 }
